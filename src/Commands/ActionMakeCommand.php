f56e60b7e0921aff8758a1a2872b246b7f529e73
Matan Yadaev <matan.yed@gmail.com>
2019-06-18 00:41:42 +0300

    remove comments from ActionMakeCommand

--- src/Commands/ActionMakeCommand.php
+++ src/Commands/ActionMakeCommand.php
@@ -7,45 +7,17 @@ use Illuminate\Console\GeneratorCommand;
 class ActionMakeCommand extends GeneratorCommand
 {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
 	protected $name = 'make:action';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
 	protected $description = 'Create a new action class';
 
-	/**
-	 * The type of class being generated.
-	 *
-	 * @var string
-	 */
 	protected $type = 'Action';
 
-	/**
-	 * Get the stub file for the generator.
-	 *
-	 * @return string
-	 */
 	protected function getStub()
 	{
         return __DIR__.'/../../stubs/action.stub';
-
     }
 
-	/**
-	 * Get the default namespace for the class.
-	 *
-	 * @param  string $rootNamespace
-	 *
-	 * @return string
-	 */
 	protected function getDefaultNamespace($rootNamespace)
 	{
 		return $rootNamespace.'\Actions';
