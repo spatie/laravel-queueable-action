c08c76d86819629048f1b841234fe36c0bac65c1
Brent Roose <brent@spatie.be>
2020-07-22 14:00:40 +0000

    Apply fixes from StyleCI

--- src/ActionJob.php
+++ src/ActionJob.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use PDO;
 
 class ActionJob implements ShouldQueue
 {
