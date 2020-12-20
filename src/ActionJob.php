c15bf0c47bfb0780f727533954749f16f880d2dd
freekmurze <freekmurze@users.noreply.github.com>
2020-12-20 14:44:11 +0000

    Fix styling

--- src/ActionJob.php
+++ src/ActionJob.php
@@ -2,12 +2,12 @@
 
 namespace Spatie\QueueableAction;
 
-use Throwable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Throwable;
 
 class ActionJob implements ShouldQueue
 {
