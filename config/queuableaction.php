cee2f6fda344534b963ca3fd138994e5de73e4e4
Chris <chrisbreuer93@gmail.com>
2022-02-08 13:59:55 -0800

    chore: fixes typo

--- config/queuableaction.php
+++ config/queuableaction.php
@@ -4,7 +4,7 @@ return [
     /*
      * The job class that will be dispatched.
      * If you would like to change it and use your own job class,
-     * it must extends the \Spatie\QueueableAction\ActionJob class.
+     * it must extend the \Spatie\QueueableAction\ActionJob class.
      */
     'job_class' => \Spatie\QueueableAction\ActionJob::class,
 ];
